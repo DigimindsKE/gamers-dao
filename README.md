1a79e9aecf460ba56e589a76de899e618ad621b2
Ibraziz21 <33128164+ibraziz21@users.noreply.github.com>
2022-10-14 16:19:02 +0300

    Add signers function completed

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# Sample Hardhat Project
-
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
+# Gamers DAO
+A ‘game engine’ sort of DAO which allows members of the ecosystem to create game contracts under it
 
 ```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
+
+Governance of this projects is based on an off chain voting system 
+Community and devs vote on the signers(delegates) who will portray their decisions on chain. They can also vote out a signer
+Game contracts will be added through the main contract, provided that the community approve it, and it matches the rules and requirements of the DAO
+For a user to contribute(voting or proposing) they have to own a portion of the DAO’s governance ERC1155 token
+Game contracts to use resources(characters/weapons/currencies etc) contracts from within the ecosystem
 ```
--- contracts/multisig.sol
+++ contracts/multisig.sol
@@ -9,47 +9,86 @@ contract multisig {
     -- a member can vote for a proposal
     -- a member can submit a game contract for review by the Audited developers
     */
- //for a user to propose an address as a signer, they have to be an approved member of the dao ecosystem
+    //for a user to propose an address as a signer, they have to be an approved member of the dao ecosystem
 
-address[] internal owners;
-mapping (address => bool) public approvedSigner;
-uint minVotes;
+    address[] internal owners;
+    //mapping to assign Approved signers
+    mapping(address => bool) public approvedSigner;
 
-modifier onlyApproved {
-    require(approvedSigner[msg.sender],"Not Authorized");
-    _;
-}
-constructor(address[] memory Addresses){
-    require(Addresses.length>0,"Not enough signers to deploy the contract");
-    owners=Addresses;
-    for (uint i = 0; i < owners.length; i++) {
-    approvedSigner[owners[i]]=true;
-}
-}
-struct proposalSigner {
-     bool voteActive;
-     uint8 votesFor;
-     uint8 votesAgainst;
-}
-
-mapping(address=>proposalSigner) private proposal;
-mapping(address=>bool) hasVoted;
-function VoteforSigner(address Address, bool Vote) external onlyApproved{
-    require(!hasVoted[msg.sender],"Already voted");
-uint8 YesCount;
-uint8 NoCount;
-if(Vote){
-    YesCount++;
-}else{
-    NoCount++;
-}
-proposal[Address].votesFor +=YesCount;
-proposal[Address].votesAgainst+=NoCount;
-}
-
-function addSignerAddress(address Address) internal {
-    if(proposal[Address].votesFor>proposal[Address].votesAgainst){
-        approvedSigner[Address]=true;
+    modifier onlyApproved() {
+        require(approvedSigner[msg.sender], "Not Authorized");
+        _;
     }
-}
+
+    //add the owner addresses as initial signers
+    constructor(address[] memory Addresses) {
+        require(
+            Addresses.length > 0,
+            "Not enough signers to deploy the contract"
+        );
+        owners = Addresses;
+        for (uint i = 0; i < owners.length; i++) {
+            approvedSigner[owners[i]] = true;
+        }
+    }
+
+    uint totalvotes = owners.length;
+    uint minVotes = totalvotes / 2 + 1;
+    struct proposalSigner {
+        bool voteActive;
+        uint8 votesFor;
+        uint8 votesAgainst;
+    }
+    //proposing a vote to add signer
+    mapping(address => proposalSigner) private proposal;
+
+    //proposing a vote to remove signer
+    mapping(address => proposalSigner) private outProposal;
+
+    mapping(address => bool) private hasVoted;
+
+    //to propose a signer, the address will have voteActive boolean set to true in order to allow voting
+    function proposeSigner(address Address) public onlyApproved {
+        require(!approvedSigner[Address], "already A signer");
+        proposal[Address].voteActive = true;
+    }
+
+    function VoteforSigner(address Address, bool Vote) external onlyApproved {
+        require(
+            proposal[Address].voteActive,
+            "Voting for this Address is Inactive"
+        );
+        require(!hasVoted[msg.sender], "Already voted");
+        uint8 YesCount;
+        uint8 NoCount;
+        if (Vote) {
+            YesCount++;
+        } else {
+            NoCount++;
+        }
+        proposal[Address].votesFor += YesCount;
+        proposal[Address].votesAgainst += NoCount;
+
+        if (proposal[Address].votesFor >= minVotes) {
+            approvedSigner[Address] = true;
+        }
+    }
+
+    //vote out  Signer function
+    function VoteOutSigner(address Address, bool Vote) external onlyApproved {
+        require(approvedSigner[Address], "Not A valid Address");
+        require(!hasVoted[msg.sender], "Already voted");
+        uint8 YesCount;
+        uint8 NoCount;
+        if (Vote) {
+            YesCount++;
+        } else {
+            NoCount++;
+        }
+        outProposal[Address].votesFor += YesCount;
+        outProposal[Address].votesAgainst += NoCount;
+        if (outProposal[Address].votesFor >= minVotes) {
+            approvedSigner[Address] = false;
+        }
+    }
 }

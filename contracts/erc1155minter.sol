0804376e50df5421073bfc456561ea8814697259
Ibraziz21 <33128164+ibraziz21@users.noreply.github.com>
2022-10-20 14:12:03 +0300

    ERC1155minter added

---
+++ contracts/erc1155minter.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.17;
+
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+
+contract Name {
+    constructor() {
+        
+    }
+}

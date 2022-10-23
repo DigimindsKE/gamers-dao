7e0e74ea211ea64dc025c45a566697705e68336b
Ibraziz21 <33128164+ibraziz21@users.noreply.github.com>
2022-10-23 10:19:06 +0300

    Update erc1155minter.sol

--- contracts/erc1155minter.sol
+++ contracts/erc1155minter.sol
@@ -3,20 +3,34 @@ pragma solidity 0.8.17;
 
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+/* 
+Contract to create new currencies
+Can only be called by the multi-sig contract.
+
+minting tokens as rewards can be called by enemies/reward contracts
+burnable
+*/
 
 
-contract erc1155Minter is ERC1155,Ownable {
+contract erc1155Minter is ERC1155, Ownable, ERC1155Burnable {
+    constructor() ERC1155("") {}
 
-
-uint newtoken;
-
-    constructor() ERC1155(""){
-        newtoken++;
-        _mint(_msgSender(),newtoken,1000000,"Governance");
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
     }
 
-    function mintNew(uint supply, bytes memory _data) external  {
-        uint tokenid=++newtoken;
-        _mint(_msgSender(),tokenid,supply,_data);
+    function mint(address account, uint256 id, uint256 amount, bytes calldata data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes calldata data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
     }
 }

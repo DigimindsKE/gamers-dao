b32dfbcb46688594d8c003899935783717406200
Ibraziz21 <33128164+ibraziz21@users.noreply.github.com>
2022-11-02 11:35:29 +0300

    Update reward.sol

--- contracts/reward.sol
+++ contracts/reward.sol
@@ -2,14 +2,24 @@
 pragma solidity 0.8.17;
 import "./erc1155currencyminter.sol";
 import "./enemies.sol";
-
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 struct RewardPool {
     uint[] tokenIDs;
     uint[] baseRewards;
 }
 
-contract rewards {
+contract rewards is VRFConsumerBaseV2{
+VRFCoordinatorV2Interface COORDINATOR;
+
+    bytes32 public keyHash;
     
+    uint16 requestConfirmations = 3;
+    uint64 subscriptionID;
+    uint256 public fee;
+    uint256 public randomResult;
+
+    address vrfCoordinator;
     address private currencyMinterAddress;
     address private managementContract;
     
@@ -18,9 +28,13 @@ contract rewards {
     uint private poolID;
     mapping(uint => RewardPool)  poolDetails;
     
-    constructor(address _currencyAddress, address _managementContract) {
+    constructor(address _currencyAddress, address _managementContract, uint64 _subscriptionID, address _vrfCoordinator, bytes32 _keyHash) VRFConsumerBaseV2(_vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
         currencyMinterAddress = _currencyAddress;
         managementContract = _managementContract;
+        subscriptionID = _subscriptionID;
+        vrfCoordinator = _vrfCoordinator;
+        keyHash=_keyHash;
     }
 
     //mintRewards can only be called by a game

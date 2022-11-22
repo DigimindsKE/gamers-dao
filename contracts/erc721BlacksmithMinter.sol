98f48eb14dd99f0b6d4fa5c4a77462c5d648079e
Ibraziz21 <33128164+ibraziz21@users.noreply.github.com>
2022-11-22 12:22:24 +0300

    Update erc721BlacksmithMinter.sol

--- contracts/erc721BlacksmithMinter.sol
+++ contracts/erc721BlacksmithMinter.sol
@@ -14,6 +14,7 @@ contract erc721BlacksmithMinter is ERC721, Ownable, RNG {
     error NotApprovedCurrency();
     error InsufficientAmount();
     error ZeroAddress();
+    error NotApprovedOperator();
 
     struct WeaponType {
         string[] typeWeapon; //axe, sword, gun, etc
@@ -26,8 +27,8 @@ contract erc721BlacksmithMinter is ERC721, Ownable, RNG {
 
     address private admin;
     address private multisigAddress;
-    multisig private dao;
-    RNG private rng;
+    //    multisig private dao;
+    //  RNG private rng;
     ICurrency private token;
     uint private wTypeCounter;
     uint private tokenCounter;
@@ -42,19 +43,20 @@ contract erc721BlacksmithMinter is ERC721, Ownable, RNG {
         address _DAO,
         address _rng,
         address _buyToken
-    ) ERC721("Weapons", "WPN") RNG(subscriptionId,vrfCoordinator,keyHash) {
-        if(_DAO==address(0)||_rng==address(0)|| _buyToken==address(0)) revert ZeroAddress();
-        dao = multisig(_DAO);
+    ) ERC721("Weapons", "WPN") RNG(subscriptionId, vrfCoordinator, keyHash) {
+        if (_DAO == address(0) || _rng == address(0) || _buyToken == address(0))
+            revert ZeroAddress();
+        // dao = multisig(_DAO);
         multisigAddress = _DAO;
-        rng = RNG(_rng);
+        // rng = RNG(_rng);
         token = ICurrency(_buyToken);
         // admin = multisig.admin();
         tokenCounter = 0;
     }
 
     function setWeaponPrice(uint tokenId, uint price) external {
-        if (!dao.currencyApproved(tokenId)) revert NotApprovedCurrency();
-        if (price == 0) revert InvalidPrice();
+        //if (!dao.currencyApproved(tokenId)) revert NotApprovedCurrency();
+        if (price == 0 || tokenId == 0) revert InvalidPrice();
         buyingCurrency = tokenId;
         buyingPrice = price;
     }
@@ -77,8 +79,10 @@ contract erc721BlacksmithMinter is ERC721, Ownable, RNG {
         //check if there is enough tokens in sender wallet
         if (token.balanceOf(_msgSender(), buyingCurrency) <= buyingPrice)
             revert InsufficientAmount();
+        if (!token.isApprovedForAll(_msgSender(), address(this)))
+            revert NotApprovedOperator();
 
-//send buying price to burn address
+        //send buying price to burn address
         token.safeTransferFrom(
             _msgSender(),
             address(0),
@@ -88,7 +92,7 @@ contract erc721BlacksmithMinter is ERC721, Ownable, RNG {
         );
         uint tokenID = ++tokenCounter;
         _safeMint(_msgSender(), tokenID);
-        uint requestId = rng.requestRandomWords(1, 200000);
+        uint requestId = requestRandomWords(1, 200000);
         requestID = requestId;
     }
 
@@ -97,9 +101,9 @@ contract erc721BlacksmithMinter is ERC721, Ownable, RNG {
         virtual
         override
     {
-        uint _requestId = requestId;
+        //uint _requestId = requestId;
         //Retrieve details for mint using request ID
-        WeaponType storage weapon = weaponType[_requestId];
+        WeaponType storage weapon = weaponType[tokenCounter];
         uint index = randomWords[0] % weapon.typeWeapon.length;
 
         MintedWeapon storage tokens = mintedDetails[tokenCounter];
@@ -112,11 +116,14 @@ contract erc721BlacksmithMinter is ERC721, Ownable, RNG {
         WeaponType storage item = weaponType[id];
 
         string[] memory toBeDeleted = item.typeWeapon;
-    
+
         for (uint i = 0; i < toBeDeleted.length; ) {
-            if (keccak256(abi.encodePacked(_weaponType)) == keccak256(abi.encodePacked(toBeDeleted[i]))) {
-                 delete toBeDeleted[i];
-               delete item.weaponImage[i];
+            if (
+                keccak256(abi.encodePacked(_weaponType)) ==
+                keccak256(abi.encodePacked(toBeDeleted[i]))
+            ) {
+                delete toBeDeleted[i];
+                delete item.weaponImage[i];
             }
             unchecked {
                 i++;

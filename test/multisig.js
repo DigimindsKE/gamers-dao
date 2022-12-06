4d7aa31acde2b663d459e8f694d9ce6c595d0518
Ibraziz21 <33128164+ibraziz21@users.noreply.github.com>
2022-12-06 13:15:42 +0300

    Update multisig.js

--- test/multisig.js
+++ test/multisig.js
@@ -4,15 +4,15 @@ const { ethers } = require("ethers");
 describe("multisig DAO", function () {
     let nftdeploy;
     beforeEach(async function () {
-        //const currencyMinter = await hre.ethers.getContractFactory("erc1155CurrencyMinter")
-
+        
         // await minter.deployed();
         [owner, user, user1, user2] = await hre.ethers.getSigners();
+        const currencyMinter = await hre.ethers.getContractFactory("erc1155CurrencyMinter");
 
         const multisig = await hre.ethers.getContractFactory("multisig");
         Multisig = await multisig.deploy([owner.address, user.address, user1.address]);
 
-        //const minter  =await currencyMinter.deploy(Multisig.address, Multisig.address);
+        Minter  =await currencyMinter.deploy(Multisig.address);
 
     })
     it("Admin should be able to propose a signer", async function () {
@@ -20,8 +20,9 @@ describe("multisig DAO", function () {
         let resp = await pSigner.wait()
         //    console.log("2: ",resp.events[0].args.voteId.toString())
         const id = resp.events[0].args.voteId.toString()
-
-        expect(await Multisig.proposal(id).voteActive == true);
+        let active = (await Multisig.proposal(1)).voteActive;
+        console.log(active);
+        expect(active).to.equal(true);
     })
 
     it("Admin should be able to propose a currency", async function () {
@@ -29,15 +30,15 @@ describe("multisig DAO", function () {
         let resp = await pcurrency.wait()    //   const id = pcurrency.id;
         const id = resp.events[0].args.voteId.toString();
                 console.log(id)
-        expect((await Multisig.currencyProposal(1).voteActive) == true);
+        expect((await Multisig.currencyProposal(1)).voteActive).to.equal(true);
     })
 
     it("Admin should be able to propose a game", async function () {
         const testAddress = "0x34d235fC47593EA72A493804FEd11C1499A7826C";
-        let pgame = await Multisig.proposeGame(testAddress);
+        await Multisig.proposeGame(testAddress);
         // console.log(pgame)
         // const id = pgame.id;
-        expect(await Multisig.gameProposal(1).voteActive == true);
+        expect((await Multisig.gameProposal(1)).voteActive).to.equal(true);
     })
 
     it("A voted in address should be a signer", async function () {
@@ -48,14 +49,39 @@ describe("multisig DAO", function () {
         console.log(id)
         //    const id = pSigner.voteID;
 
-        await Multisig.connect(owner).VoteforSigner(id, false);
-        await Multisig.connect(user1).VoteforSigner(id, false);
-       // await Multisig.connect(user).VoteforSigner(id, true);
+        await Multisig.connect(owner).VoteforSigner(1, true);
+        await Multisig.connect(user1).VoteforSigner(1, true);
+      // await Multisig.connect(user).VoteforSigner(id, true);
 
         //  await Multisig.connect(user1).VoteforSigner(id, true);
-
-        expect((await Multisig.approvedSigner(user2.address)) == true);
-
+            console.log(await Multisig.signers(0));
+            console.log(await Multisig.signers(1));
+            console.log(await Multisig.signers(2));
+           
+        expect(await Multisig.approvedSigner(user2.address)).to.equal(true);
+        console.log(await Multisig.approvedSigner(user2.address));
     })
 
+    it("A voted currency should be minted", async function () {
+        await Multisig.proposeCurrency(100000);
+        await Multisig.setMinterAddress(Minter.address);
+        await Multisig.connect(user1).voteCurrency(1, true);
+        
+        await Multisig.connect(user).voteCurrency(1, true);
+        
+        
+        console.log((await Minter.balanceOf(Multisig.address,1)).toString());
+        expect((await Minter.balanceOf(Multisig.address,1)).toString()).to.equal('100000');
+    })
+
+
+        it("A voted in game should be allowed", async function(){
+            const testAddress = "0x34d235fC47593EA72A493804FEd11C1499A7826C";
+            await Multisig.proposeGame(testAddress);
+
+            await Multisig.connect(user).voteForGame(1, true);
+            await Multisig.connect(user1).voteForGame(1, true);
+
+            expect(await Multisig.gameApproved(testAddress)) .to.equal(true);
+        })
 })

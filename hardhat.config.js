6ba3f6d29da7020d4095ba3e56d256871ab56867
Ibrahim Aziz <ibraziz21@gmail.com>
2022-12-23 22:20:31 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+
+  networks:{
+    goerli: {
+      url: process.env.URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY
+  }
 };
